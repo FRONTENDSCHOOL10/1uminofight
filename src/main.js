79da1645abe881e58742e911970982a915506826
arayofseonshine <arayofseonshine@naver.com>
2024-07-10 22:43:54 +0900

    1차 배포 테스트

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ import '/src/pages/productDetail/heartIcon.js';
 import '/src/pages/productList/numbering.js';
 import '/src/components/navMenu.js';
 import '/src/components/navMenuItem.js'
+import '/src/components/headerCategory.js';
+import '/src/components/headerCategoryItemList.js'
