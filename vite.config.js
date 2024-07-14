4e3679faf8a8758ae2ed0e91307922bb72d1efe1
paintover <tjdus1139@naver.com>
2024-07-14 14:54:50 +0900

    Test: netlify 경로 테스트
    * 변경사항
    - netlify.toml 삭제
    - _redirects 삭제
    - vite.config.js에 productList 코드 추가

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir:'docs',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-		login: resolve(__dirname, './src/pages/login/login.html'),
+        login: resolve(__dirname, './src/pages/login/login.html'),
+        productList: resolve(
+          __dirname,
+          './src/pages/productList/productList.html'
+        ),
       },
     },
-
   },
-})
+});
