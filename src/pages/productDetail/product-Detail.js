888fe69497b7aaa5af156ce1c3101a0a31339a70
arayofseonshine <arayofseonshine@naver.com>
2024-07-18 10:24:08 +0900

    Feat : productDetailPage 조립_#69

--- src/pages/productDetail/product-Detail.js
+++ src/pages/productDetail/product-Detail.js
@@ -1,7 +1,6 @@
-import '/src/components/Header.js';
+
 import '/src/pages/productDetail/product-count.js';
 import '/src/pages/productDetail/heartIcon.js';
 import '/src/components/top-banner.js';
 import '/src/components/footer/footer.js';
-
-
+import '/src/components/Header.js';
